7941d8eaf641d9c41b0ab3e0ac4c24a51029c765
subintuladhar17 <subintuladhar17@gmail.com>
2024-04-20 13:11:46 +0545

    Hello world

--- src/Roouts/Approutes.jsx
+++ src/Roouts/Approutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 
 import Home from "../Pages/Home"
 import About from "../Pages/About"
@@ -7,7 +7,8 @@ import About from "../Pages/About"
 const Approutes = () => {
     return (
         <div>
-
+            <BrowserRouter>
+            
             <Router>
                 <Routes>
                     <Route path='/' element={<Home />}></Route>
@@ -15,6 +16,8 @@ const Approutes = () => {
                 </Routes>
 
             </Router>
+            </BrowserRouter>
+
 
         </div>
     )
